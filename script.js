93a299c94d4462a1db3fbc8bf8c8e5097b36bd3b
Abhilash Kumar Giri <114879092+Abhi-lash19@users.noreply.github.com>
2024-05-14 11:42:42 +0000

    modified js file for user event

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turn = "X"
+console.log("Welcome to Tic Tac Triumph");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
 let isgameover = false;
 
 // Function to change the turn
-const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+const changeTurn = () => {
+    return turn === "X"? "0" : "X";
 }
 
 // Function to check for a win
-const checkWin = ()=>{
+const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
@@ -22,46 +22,75 @@ const checkWin = ()=>{
         [2, 5, 8, 15, 15, 90],
         [0, 4, 8, 5, 15, 45],
         [2, 4, 6, 5, 15, 135],
-    ]
-    wins.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
-            isgameover = true
+    ];
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
+            (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && 
+            (boxtext[e[0]].innerText!== "")) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            isgameover = true;
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
             document.querySelector(".line").style.width = "20vw";
+            document.querySelector(".line").style.backgroundColor = "#82E0AA"; /* Light green */
+            document.querySelector(".line").animate([{ transform: 'scale(1)' }, { transform: 'scale(1.2)' }], { duration: 500, fill: 'forwards' });
+            gameover.play(); // Play the game over sound
         }
-    })
+    });
 }
 
 // Game Logic
-// music.play()
+document.getElementById('playMusic').addEventListener('click', () => {
+    music.play();
+});
+
+document.getElementById('stopMusic').addEventListener('click', () => {
+    music.pause();
+    music.currentTime = 0; // Reset the music to the beginning
+});
+
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '' &&!isgameover) {
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
         }
-    })
-})
+    });
+});
 
 // Add onclick listener to reset button
-reset.addEventListener('click', ()=>{
+document.getElementById('reset').addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
+        element.innerText = "";
     });
-    turn = "X"; 
-    isgameover = false
+    turn = "X";
+    isgameover = false;
     document.querySelector(".line").style.width = "0vw";
-    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+});
 
+// Add onclick listener to settings button
+document.getElementById('settingsButton').addEventListener('click', function() {
+    document.getElementById('settingsModal').style.display = 'block';
+});
+
+// Close modal functionality
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('settingsModal').style.display = 'none';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('themeToggle');
+    themeToggle.addEventListener('click', function() {
+        document.body.classList.toggle('dark');
+    });
+});
